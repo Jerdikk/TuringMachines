19f968dcea1863ffe5c1eb5f64aed49046f36ffc
Marat Fazylyanov <maratf76@gmail.com>
2016-12-19 10:17:14 +0500

    XML 0.2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 // Turnig.cpp: определяет точку входа для консольного приложения.
 //
+/*
+Original code by Marat Fazylyanov 
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any
+damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any
+purpose, including commercial applications, and to alter it and
+redistribute it freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must
+not claim that you wrote the original software. If you use this
+software in a product, an acknowledgment in the product documentation
+would be appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and
+must not be misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source
+distribution.
+*/
 
 #include "stdafx.h"
 //#include <std
@@ -93,8 +115,10 @@ void tMachine::SaveXML()
 	int i;
 	TiXmlDocument doc;
 	TiXmlElement * xmlroot;
-	TiXmlElement * xmlAlphabet;
+	TiXmlElement * xmlAlphabet;	
 	TiXmlElement * xmlSymbol;
+	TiXmlElement * xmlRules;
+	TiXmlElement * xmlRule;
 	char tempCStr[2];	
 
 	doc.LinkEndChild( new TiXmlDeclaration( "1.0", "", "" ) );
@@ -115,8 +139,17 @@ void tMachine::SaveXML()
 		xmlAlphabet->LinkEndChild( xmlSymbol );
 	}
 
-
+	xmlRules = new TiXmlElement( "Rules" );
+	xmlroot->LinkEndChild( xmlRules );
 	
+	//CurentSymbol
+	//CurrentState
+	//SymbolToWrite
+	//NextState
+	//Direction
+	//IsRule
+
+
 	doc.SaveFile( "madeByHand.xml" );
 
 }
